76eaee5bca853e959cef15c0d98f37b61389230c
Kawrona <karol.wrona01@gmail.com>
2024-04-27 20:33:06 +0200

    black reformat

--- src/irace_test.py
+++ src/irace_test.py
@@ -4,13 +4,13 @@ import sys
 # read the --paremreal= flag
 paramreal = 0.0
 for arg in sys.argv:
-    if arg.startswith('--paramreal='):
-        paramreal = float(arg[len('--paramreal='):])
+    if arg.startswith("--paramreal="):
+        paramreal = float(arg[len("--paramreal=") :])
 
 # read the --x flag
 x = 10
 for arg in sys.argv:
-    if arg.startswith('--x'):
-        x = int(arg[len('--x'):])
+    if arg.startswith("--x"):
+        x = int(arg[len("--x") :])
 
-print(paramreal / x, end='')
+print(paramreal / x, end="")
